9bc10668b017059747003a21d5e773d16ff07f24
abimont <abimontes00@gmail.com>
2023-01-12 20:46:27 -0500

    Task 4

---
+++ module1_task4/content/posts/welcome.md
@@ -0,0 +1,6 @@
+---
+title: "Welcome to Awesome Inc."
+date: 2023-01-09T18:16:41-05:00
+draft: false
+---
+
---
+++ module1_task4/setup.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+apt-get update
+apt-get install -y hugo make
+apt install curl
+curl -L https://github.com/gohugoio/hugo/releases/download/v0.109.0/hugo_extended_0.109.0_linux-amd64.deb -o hugo.deb
+apt install -y ./hugo.deb
+make build
