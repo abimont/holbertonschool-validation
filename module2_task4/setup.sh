7281274794ddd09fafa249ab32b88d3cb63b6a67
abimont <abimontes00@gmail.com>
2023-01-20 16:17:22 -0500

    Setup task 4

--- module2_task4/setup.sh
+++ module2_task4/setup.sh
@@ -1,6 +1,7 @@
-#!/usr/bin/env bash
-apt-get update && apt-get install -y make wget
-wget https://github.com/gohugoio/hugo/releases/download/v0.109.0/hugo_extended_0.109.0_linux-amd64.deb
-dpkg -i hugo_extended_0.109.0_linux-amd64.deb
-rm -f hugo_extended_0.109.0_linux-amd64.deb
+#!/bin/bash
+apt-get update
+apt-get install -y hugo make
+apt-get install curl -y
+curl -Lo install_hugo.deb https://github.com/gohugoio/hugo/releases/download/v0.109.0/hugo_extended_0.109.0_linux-amd64.deb
+apt-get install ./install_hugo.deb
 make build
